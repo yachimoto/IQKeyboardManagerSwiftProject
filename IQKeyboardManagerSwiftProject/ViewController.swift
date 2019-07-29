42713ac928fad6e38d9366961672ffac967b3eb3
yachimoto <yachimoto0607@gmail.com>
2019-07-29 09:43:08 +0800

    Initial Commit

---
+++ IQKeyboardManagerSwiftProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IQKeyboardManagerSwiftProject
+//
+//  Created by SeitoYachimoto on 2019/07/29.
+//  Copyright © 2019 谷地元　星斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
